003d17c84bd70f68850358301dd1c7a0f0cf54d9
aymen2782 <aymen.sellaouti@gmail.com>
2022-02-05 12:46:52 +0100

    card exercice part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { FirstComponent } from './component/first/first.component';
 import { SecondComponent } from './component/second/second.component';
 import { ColorComponent } from './component/color/color.component';
+import {FormsModule} from "@angular/forms";
+import { TwoComponent } from './component/two/two.component';
+import { CardComponent } from './component/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
     SecondComponent,
-    ColorComponent
+    ColorComponent,
+    TwoComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
